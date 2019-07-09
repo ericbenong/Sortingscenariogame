4d4a4a7b1e5828c8fd7e9d6ea50ca682779a78bb
ericbenong <eric@ericbenson.co>
2019-07-09 13:36:26 -0500

    sortingscenariogame

---
+++ story_content/user.js
@@ -0,0 +1,91 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5WpTw3DzmHo":
+        Script1();
+        break;
+      case "6MVU01FOrGi":
+        Script2();
+        break;
+      case "6N2YbxnlMBo":
+        Script3();
+        break;
+      case "5urwSCOhAjr":
+        Script4();
+        break;
+      case "6TTAsQ49MKs":
+        Script5();
+        break;
+      case "68St8N46f1P":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  var randomnumber1 = Math.floor((Math.random()*4)-2);
+var randomnumber2 = Math.floor((Math.random()*4)-2);
+var randomnumber3 = Math.floor((Math.random()*4)-2);
+var player = GetPlayer();
+player.SetVar("randnum1",randomnumber1)
+player.SetVar("randnum2",randomnumber2)
+player.SetVar("randnum3",randomnumber3)
+}
+
+function Script2()
+{
+  var randomnumber1 = Math.floor((Math.random()*4)-2);
+var randomnumber2 = Math.floor((Math.random()*4)-2);
+var randomnumber3 = Math.floor((Math.random()*4)-2);
+var player = GetPlayer();
+player.SetVar("randnum1",randomnumber1)
+player.SetVar("randnum2",randomnumber2)
+player.SetVar("randnum3",randomnumber3)
+}
+
+function Script3()
+{
+  var randomnumber1 = Math.floor((Math.random()*4)-2);
+var randomnumber2 = Math.floor((Math.random()*4)-2);
+var randomnumber3 = Math.floor((Math.random()*4)-2);
+var player = GetPlayer();
+player.SetVar("randnum1",randomnumber1)
+player.SetVar("randnum2",randomnumber2)
+player.SetVar("randnum3",randomnumber3)
+}
+
+function Script4()
+{
+  var randomnumber1 = Math.floor((Math.random()*4)-2);
+var randomnumber2 = Math.floor((Math.random()*4)-2);
+var randomnumber3 = Math.floor((Math.random()*4)-2);
+var player = GetPlayer();
+player.SetVar("randnum1",randomnumber1)
+player.SetVar("randnum2",randomnumber2)
+player.SetVar("randnum3",randomnumber3)
+}
+
+function Script5()
+{
+  var randomnumber1 = Math.floor((Math.random()*4)-2);
+var randomnumber2 = Math.floor((Math.random()*4)-2);
+var randomnumber3 = Math.floor((Math.random()*4)-2);
+var player = GetPlayer();
+player.SetVar("randnum1",randomnumber1)
+player.SetVar("randnum2",randomnumber2)
+player.SetVar("randnum3",randomnumber3)
+}
+
+function Script6()
+{
+  var randomnumber1 = Math.floor((Math.random()*4)-2);
+var randomnumber2 = Math.floor((Math.random()*4)-2);
+var randomnumber3 = Math.floor((Math.random()*4)-2);
+var player = GetPlayer();
+player.SetVar("randnum1",randomnumber1)
+player.SetVar("randnum2",randomnumber2)
+player.SetVar("randnum3",randomnumber3)
+}
+
