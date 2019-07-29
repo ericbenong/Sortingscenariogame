d4ce2406acbd8c315011f41f892bf3e4cff38896
ericbenong <eric@ericbenson.co>
2019-07-29 16:53:54 -0500

    Update 7/29
    update 7/29

--- story_content/user.js
+++ story_content/user.js
@@ -2,36 +2,29 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5WpTw3DzmHo":
+      case "6IX5utTSYYi":
         Script1();
         break;
-      case "6MVU01FOrGi":
+      case "6CMBj4nmFPE":
         Script2();
         break;
-      case "6N2YbxnlMBo":
+      case "68453sJCjAl":
         Script3();
         break;
-      case "5urwSCOhAjr":
+      case "5Wz4q1SuJSe":
         Script4();
         break;
-      case "6TTAsQ49MKs":
+      case "6OlWUDhnK1S":
         Script5();
-        break;
-      case "68St8N46f1P":
-        Script6();
         break;
   }
 }
 
 function Script1()
 {
-  var randomnumber1 = Math.floor((Math.random()*4)-2);
-var randomnumber2 = Math.floor((Math.random()*4)-2);
-var randomnumber3 = Math.floor((Math.random()*4)-2);
+  var randomnumber = Math.floor((Math.random()*4)+1);
 var player = GetPlayer();
-player.SetVar("randnum1",randomnumber1)
-player.SetVar("randnum2",randomnumber2)
-player.SetVar("randnum3",randomnumber3)
+player.SetVar("randnum",randomnumber);
 }
 
 function Script2()
@@ -68,17 +61,6 @@ player.SetVar("randnum3",randomnumber3)
 }
 
 function Script5()
-{
-  var randomnumber1 = Math.floor((Math.random()*4)-2);
-var randomnumber2 = Math.floor((Math.random()*4)-2);
-var randomnumber3 = Math.floor((Math.random()*4)-2);
-var player = GetPlayer();
-player.SetVar("randnum1",randomnumber1)
-player.SetVar("randnum2",randomnumber2)
-player.SetVar("randnum3",randomnumber3)
-}
-
-function Script6()
 {
   var randomnumber1 = Math.floor((Math.random()*4)-2);
 var randomnumber2 = Math.floor((Math.random()*4)-2);
